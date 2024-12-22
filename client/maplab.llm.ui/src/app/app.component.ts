89be32197fdd73c50d62d93028c355be58f0bfed
Yasser Kharab <kharabyasser@icloud.com>
2024-12-22 17:11:23 -0500

    up

--- client/maplab.llm.ui/src/app/app.component.ts
+++ client/maplab.llm.ui/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterModule],
+  selector: 'maplab-chat',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'maplab.llm.ui';
